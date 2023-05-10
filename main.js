8f81df2b511ac867deef1db89c2171b2bf7f444c
PedroHzd <pediaz333@gmail.com>
2023-05-10 17:42:15 -0500

    Added several functions

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 const blocks = document.querySelectorAll('.containerChildren');
+// const containerRow = document.createElement('div');
+const container = document.querySelector('.container');
+const containerRow = document.querySelector('.containerRow');
+
+let creatingNewRows = (numberOfRows) => {
+    for (let i = 0; i < numberOfRows; i++) {
+        let containerRow = document.createElement('div');
+        container.appendChild(containerRow);
+        containerRow.classList.toggle('containerRow');
+    }
+};
+
+let creatingNewBlocks = (numberOfBlocks) => {
+    for (let i = 0; i < numberOfBlocks; i++) {
+        let individualBlock = document.createElement('div');
+        containerRow.appendChild(individualBlock);
+        individualBlock.classList.toggle('containerChildren');
+    }
+};
+
+let calculateWidthHeight = (numberOfBlocks) => {
+    let widthAndHeight = 0;
+    widthAndHeight = (1000 - (numberOfBlocks + 1)) / numberOfBlocks;
+    return widthAndHeight;
+};
+
+
+
 
 blocks.forEach((block) => {
-    block.addEventListener('click', () => {
+    block.addEventListener('mouseover', () => {
         block.style.backgroundColor = 'white';
     });
 });
+
+console.log([...blocks]);
